11493dd4d1244bf4cfb1e4edb97dfcd00cc6d4e8
Gaetan Lepage <gaetan@glepage.com>
2023-12-06 11:06:29 +0100

    flake.nix: refactoring

--- flake.nix
+++ flake.nix
@@ -8,33 +8,23 @@
     system = "x86_64-linux";
     pkgs = import nixpkgs {inherit system;};
   in {
-    apps.${system} = {
-      default = {
-        type = "app";
-        program = let
-          serve-script = pkgs.writeShellApplication {
-            name = "serve";
-            runtimeInputs = [pkgs.hugo];
-            text = "hugo serve";
-          };
-        in "${serve-script}/bin/serve";
+    packages.${system} = with pkgs; {
+      default = writeShellApplication {
+        name = "serve";
+        runtimeInputs = [hugo];
+        text = "hugo serve";
       };
 
-      deploy = {
-        type = "app";
-        program = let
-          deploy-script = pkgs.writeShellApplication {
-            name = "deploy";
-            runtimeInputs = with pkgs; [hugo rsync];
-            text = ''
-              hugo
+      deploy = writeShellApplication {
+        name = "deploy";
+        runtimeInputs = [hugo rsync];
+        text = ''
+          hugo
 
-              rsync -rv --delete \
-                public/ \
-                server:/var/www/personal_website/
-            '';
-          };
-        in "${deploy-script}/bin/deploy";
+          rsync -rv --delete \
+            public/ \
+            server:/var/www/personal_website/
+        '';
       };
     };
   };
