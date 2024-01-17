26e01d6e9499f7580f82ceb0399a1d0f875bdfa8
Gaetan Lepage <gaetan@glepage.com>
2024-01-17 15:34:10 +0100

    flake: switch to devshell

--- flake.nix
+++ flake.nix
@@ -1,31 +1,49 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
-  outputs = {
-    self,
-    nixpkgs,
-  }: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {inherit system;};
-  in {
-    packages.${system} = with pkgs; {
-      default = writeShellApplication {
-        name = "serve";
-        runtimeInputs = [hugo];
-        text = "hugo serve";
-      };
-
-      deploy = writeShellApplication {
-        name = "deploy";
-        runtimeInputs = [hugo rsync];
-        text = ''
-          hugo
-
-          rsync -rv --delete \
-            public/ \
-            server:/var/www/personal_website/
-        '';
-      };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        inputs.devshell.flakeModule
+      ];
+
+      systems = ["x86_64-linux"];
+
+      perSystem = {
+        config,
+        pkgs,
+        ...
+      }: {
+        formatter = pkgs.alejandra;
+        devshells.default = {
+          packages = with pkgs; [
+            hugo
+            rsync
+          ];
+
+          commands = [
+            {
+              name = "deploy";
+              command = ''
+                hugo
+
+                rsync -rv --delete \
+                  public/ \
+                  server:/var/www/personal_website/
+              '';
+            }
+            {
+              name = "serve";
+              command = "hugo serve";
+            }
+          ];
+        };
+      };
+    };
 }
